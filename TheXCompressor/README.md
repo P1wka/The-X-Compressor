42fd05c0695a45eda37556a8b55479a25314e4b0
piw <grove.familia09@gmail.com>
2026-03-29 00:46:07 +0300

    v0.07

---
+++ TheXCompressor/README.md
@@ -0,0 +1,7 @@
+# The-X-Compressor
+A versital data compressor written in C#
+
+## Purposes
+- It sorts the data file according to its type.
+- It automatically selects the most useful algorithm, or the user can choose the algorithm they want.
+- Data is compressed
---
+++ TheXCompressor/TheXCompressor/Algorithms/ICompression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheXCompressor.Algorithms
+{
+    public interface ICompression
+    {
+        string Name { get; }
+
+        string Compress(string input);
+
+        string Decompress(string input);
+    }
+}
