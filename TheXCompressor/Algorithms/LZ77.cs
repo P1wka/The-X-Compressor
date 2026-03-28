4d7f1f62db6e11f62b2782f0b9a76a41a782b518
piw <grove.familia09@gmail.com>
2026-03-29 00:49:03 +0300

    v0.07

---
+++ TheXCompressor/Algorithms/LZ77.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Text;
+
+namespace TheXCompressor.Algorithms
+{
+    public class LZ77 : ICompression
+    {
+        public string Name => "LZ77";
+
+        private int windowSize = 20;
+
+        public string Compress(string input)
+        {
+            if(string.IsNullOrEmpty(input)) return "";
+
+            var output = new StringBuilder();
+            int pos = 0;
+
+            while(pos < input.Length)
+            {
+                int matchLength = 0;
+                int matchDistance = 0;
+
+                int start = Math.Max(0, pos - windowSize);
+
+                for(int i = start; i < pos; i++)
+                {
+                    int length = 0;
+                    while(pos + length < input.Length && input[i + length] == input[pos + length])
+                    {
+                        length++;
+                        if(i + length >= pos)
+                        {
+                            break;
+                        }
+                    }
+
+                    if(length > matchLength)
+                    {
+                        matchLength = length;
+                        matchDistance = pos - i;
+                    }
+                }
+
+                if (matchLength > 1)
+                {
+                    output.Append($"({matchDistance},{matchLength})");
+                    pos += matchLength;
+                }
+                else
+                {
+                    output.Append(input[pos]);
+                    pos++;
+                }
+            }
+
+            return output.ToString();
+        }
+
+        public string Decompress(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return "";
+
+            var output = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    int j = i + 1;
+                    while (input[j] != ')') j++;
+                    var token = input.Substring(i + 1, j - i - 1);
+                    var parts = token.Split(',');
+                    int distance = int.Parse(parts[0]);
+                    int length = int.Parse(parts[1]);
+
+                    int start = output.Length - distance;
+                    for (int k = 0; k < length; k++)
+                        output.Append(output[start + k]);
+
+                    i = j;
+                }
+                else
+                {
+                    output.Append(input[i]);
+                }
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ TheXCompressor/Core/CompressionManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+using TheXCompressor.Algorithms;
+
+namespace TheXCompressor.Core
+{
+    public class CompressionResult
+    {
+        public string Data { get; set; }
+        public string Algorithm { get; set; }
+        public int OriginalSize { get; set; }
+        public int CompressedSize { get; set; }
+        public double Ratio { get; set; }
+        public long TimeMs { get; set; }
+    }
+
+    public class CompressionManager
+    {
+        private List<ICompression> _algorithms;
+
+        public CompressionManager()
+        {
+            _algorithms = new List<ICompression>
+            {
+                new RLE(),
+                new Huffman()
+            };
+        }
+
+        public CompressionResult Compress(string data, string algorithmName)
+        {
+            var algo = _algorithms.FirstOrDefault(a => a.Name == algorithmName);
+
+            if (algo == null)
+                throw new Exception("Algorithm not found");
+
+            return RunCompression(algo, data);
+        }
+        
+        public CompressionResult CompressAuto(string data)
+        {
+            CompressionResult bestResult = null;
+
+            foreach (var algo in _algorithms)
+            {
+                var result = RunCompression(algo, data);
+
+                if (bestResult == null || result.CompressedSize < bestResult.CompressedSize)
+                {
+                    bestResult = result;
+                }
+            }
+
+            return bestResult;
+        }
+
+        private CompressionResult RunCompression(ICompression algo, string data)
+        {
+            var sw = Stopwatch.StartNew();
+
+            var compressed = algo.Compress(data);
+
+            sw.Stop();
+
+            return new CompressionResult
+            {
+                Data = compressed,
+                Algorithm = algo.Name,
+                OriginalSize = data.Length,
+                CompressedSize = compressed.Length,
+                Ratio = CalculateRatio(data.Length, compressed.Length),
+                TimeMs = sw.ElapsedMilliseconds
+            };
+        }
+
+        private double CalculateRatio(int original, int compressed)
+        {
+            return (1.0 - (double)compressed / original) * 100;
+        }
+    }
+}
